5627f7e0e64e2d9e79e0596e7e39f46dbb14bc88
elcrispapuh18 <elcrispapuh123@gmail.com>
2024-03-01 11:10:55 -0600

    video 1_4

---
+++ src/app/store/[category]/page.tsx
@@ -0,0 +1,23 @@
+interface CategoryProps{
+
+
+params: {
+
+    category: string
+
+}
+
+}
+
+export default function Category (props: CategoryProps){
+
+    console.log(props)
+
+    return(
+
+<h1>Categoria dinamica: category </h1>
+
+    )
+
+
+}
---
+++ src/app/store/page.tsx
@@ -0,0 +1,10 @@
+export default function Store (){
+
+
+return(
+    
+<h1>Store</h1>
+
+    )
+    
+}
